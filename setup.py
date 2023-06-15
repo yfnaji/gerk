5336a408db421708c3da92bde911bb10af49f31b
Yasser Naji <yfnaji@gmail.com>
2023-06-15 20:02:38 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+VERSION = "0.0.1"
+DESCRIPTION = "Generalized Explicit Runge-Kutta"
+
+setup(
+    name="gerk",
+    version=VERSION,
+    author="Yasser Naji",
+    author_email="yfnaji@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["matplotlib"],
+    keywords=["runge-kutta", "runge", "kutta", "numerical", "integration", "approximation"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
